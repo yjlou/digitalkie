baa1a6dac47dcba738b9b3c092a4f6a72a0bb6d7
Yung-Chieh Lo <yjlou.tw@gmail.com>
2018-11-11 15:18:51 -0800

    Regine the LoRa receiving loop: pull all until empty.

--- scripts/main.py
+++ scripts/main.py
@@ -13,13 +13,16 @@ def frame_received(data):
 
 lora_ctl = lora.LoRaController()
 spk = speaker.Speaker(DAC('P22'))
-adc = ADC() 
+adc = ADC()
 apin = adc.channel(pin='P13')
 uphone = microphone.Microphone(apin, frame_received)
 tlk_btn = talk_button.TalkButton(uphone)
 
 while True:
-  Timer.sleep_us(10000)
-  data = lora_ctl.recv()
-  if data:
-    spk.enque(data)
+  Timer.sleep_us(1000)
+  while True:
+    data = lora_ctl.recv()
+    if data:
+      spk.enque(data)
+  else:
+      break
