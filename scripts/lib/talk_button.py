58612459df4e6a43d59c13ff223b49c25e86f48f
Yung-Chieh Lo <yjlou.tw@gmail.com>
2018-11-11 19:59:17 -0800

    talk_button: add debounce logic.

--- scripts/lib/talk_button.py
+++ scripts/lib/talk_button.py
@@ -1,6 +1,7 @@
 """Controller for talk button."""
 import pycom
 from machine import Pin
+from machine import Timer
 
 
 class TalkButton(object):
@@ -14,19 +15,41 @@ class TalkButton(object):
       microphone: an microphone.Microphone object
     """
     self.pin_ = Pin(self.PIN_STR, mode=Pin.IN, pull=Pin.PULL_DOWN)
-    self.pin_.callback(Pin.IRQ_RISING, self.pressed)
-    self.pin_.callback(Pin.IRQ_FALLING, self.released)
+    self.pin_.callback(Pin.IRQ_RISING | Pin.IRQ_FALLING, self.toggled)
 
     pycom.heartbeat(False)  # RGB LED
 
     self.microphone_ = microphone
+    self.pressed_ = False
 
   def pressed(self):
-    print("Pressed ...")
+    if self.pressed_:
+      return
+
+    Timer.sleep_us(100000)  # debounce
+    if not self.pin_():
+      return
+
+    self.pressed_ = True
     pycom.rgbled(0x7f0000)  # red
     self.microphone_.start()
+    print("Pressed ...")
 
   def released(self):
-    print("Released ...")
+    if not self.pressed_:
+      return
+
+    Timer.sleep_us(100000)  # debounce
+    if self.pin_():
+      return
+
+    self.pressed_ = False
     pycom.rgbled(0x000000)  # off
     self.microphone_.stop()
+    print("Released ...")
+
+  def toggled(self, arg):
+    if self.pin_():
+      self.pressed()
+    else:
+      self.released()
