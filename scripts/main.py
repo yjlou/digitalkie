ff184efc99dfce7245e67a29a1b0d3759c95aaea
Yung-Chieh Lo <yjlou.tw@gmail.com>
2018-11-14 00:07:33 -0800

    Refactor to main().

--- scripts/main.py
+++ scripts/main.py
@@ -8,27 +8,30 @@ import microphone
 import speaker
 import talk_button
 
-def frame_received(data):
-  print('.')
-  lora_ctl.send(data)
+def main():
+  def audio_buffer(data):
+      print('.')
+      lora_ctl.send(data)
 
-lora_ctl = lora.LoRaController()
-spk = speaker.Speaker(DAC('P22'))
-adc = ADC()
-apin = adc.channel(pin='P13')
-uphone = microphone.Microphone(apin, frame_received)
-tlk_btn = talk_button.TalkButton(uphone)
+  lora_ctl = lora.LoRaController()
+  spk = speaker.Speaker(DAC('P22'))
+  adc = ADC()
+  apin = adc.channel(pin='P13')
+  uphone = microphone.Microphone(apin, audio_buffer)
+  tlk_btn = talk_button.TalkButton(uphone)
 
-print('Started ...')
-pycom.rgbled(0x007f00)  # green
-Timer.sleep_us(100000)
-pycom.rgbled(0x000000)  # off
+  print('Started ...')
+  pycom.rgbled(0x007f00)  # green
+  Timer.sleep_us(100000)
+  pycom.rgbled(0x000000)  # off
 
-while True:
-  Timer.sleep_us(1000)
   while True:
-    data = lora_ctl.recv()
-    if data:
-      spk.enque(data)
-  else:
-      break
+    Timer.sleep_us(1000)
+    while True:
+      data = lora_ctl.recv()
+      if data:
+        spk.enque(data)
+    else:
+        break
+
+main()
